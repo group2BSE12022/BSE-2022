2dbb5b3ed9fc6b54a68f9220107449fb28927aa0
group2BSE12022 <bsegrouptwo22@gmail.com>
2022-02-11 21:15:33 +0300

    .

--- src/Chapter 3/exercise 7.py
+++ src/Chapter 3/exercise 7.py
@@ -6,28 +6,28 @@ gh = "Mbale"
 ks = "Kasese"
 ar = "Arua"
 try:
-    if location == "Mbarara":
+    if location == "Mbarara" or "mbarara":
         pa = input ("Pay:")
         p = int (pa)
         if p >= 4000000:
             print ("Decision: I'll take the Mbarara job")
         else:
             print ("No thanks, I can find something better in kla")
-    elif location == "Kampala" :
+    elif location == "Kampala" or "kampala" :
         pa = input ("Pay:")
         p = int (pa)
         if p >= 10000000:
             print ("Decision: I'll take the Kampala job")
         else:
             print ("Get serious")
-    elif location == "Space":
+    elif location == "Space" or "space":
         pa = input ("Pay:")
         p = int (pa)
         if p >= 100:
             print ("Decision: I will definately take the job")
         else:
             print ("I,ll still take the job bro.")
-    elif location == gh :
+    else:
         pa = input ("Pay:")
         p = int (pa)
         if p >= 6000000:
